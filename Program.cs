7135869cec2accf94b049fff06ff0cd72e85eb65
Mary Toth <maryarylynn@gmail.com>
2021-04-29 16:45:18 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RhythmsGonnaGetYou.Models;
+using System.Linq;
+
+namespace RhythmsGonnaGetYou
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
